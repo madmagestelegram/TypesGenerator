401b6d9ab3f2542df0fe8744f58a2fa5419ffc51
a.kalin <maddmages@gmail.com>
2021-05-16 02:11:37 +0300

    fix serializer + rename abstract input file

--- src/Dictionary/Classes.php
+++ src/Dictionary/Classes.php
@@ -11,7 +11,7 @@ class Classes
     public const INLINE_QUERY_RESULT = 'AbstractInlineQueryResult';
     public const INPUT_MESSAGE_CONTENT = 'AbstractInputMessageContent';
 
-    public const INPUT_FILE = 'AbstractInputFile';
+    public const INPUT_FILE = 'InputFile';
 
     public const ABSTRACT_TYPE = 'AbstractType';
     public const TELEGRAM_EXCEPTION = 'TelegramException';
